defbaab22541a162e80afb5cf4b0efa2d043407d
zll0921 <73374236+zll0921@users.noreply.github.com>
2021-09-07 09:36:51 +0800

    Create index.vue

---
+++ src/components/Order/index.vue
@@ -0,0 +1,8 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-09-07 09:36:09
+ * @LastEditTime: 2021-09-07 09:36:43
+ * @LastEditors: LAPTOP-CC091CC3
+ * @Description: In User Settings Edit
+ * @FilePath: \houtai-ej\src\components\Order\index.vue
+-->
